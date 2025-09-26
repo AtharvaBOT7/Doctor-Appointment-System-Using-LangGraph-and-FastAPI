c34a1eb7d1e4727a848de7b2ffc58b8197bf4f8a
atharvaBOT7 <atharvachundurwar841@gmail.com>
2025-09-25 19:35:30 -0700

    LLMModel class imlemented

---
+++ utils/llms.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+
+class LLMModel:
+    def __init__(self, model_name="llama-3.3-70b-versatile"):
+        if not model_name:
+            raise ValueError("Model is not defined.")
+        
+        self.model_name = model_name
+        self.groq_model = ChatGroq(model=self.model_name)
+
+    def get_model(self):
+        return self.groq_model
+
+
+if __name__ == "__main__":
+    llm_instance = LLMModel()
+    llm_model = llm_instance.get_model()
+    
+    response = llm_model.invoke("hi")
+    print(response)
